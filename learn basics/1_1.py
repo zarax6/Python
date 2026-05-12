253d48381495a8cc6b0870a2f20f47a0a5116f53
zarax6 <mflame670@gmail.com>
2026-05-12 14:36:28 +0300

    12/05/26
    working with strings_1

--- learn basics/1_1.py
+++ learn basics/1_1.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+''' working with strings''' 
+''' 1 '''
+message = "Hello, Amigos!"
+print(message)
+
+friends = 'goths'
+print(friends)
+
+test_1 = "Console_log: \n\"Hello = Amigos!"
+print(test_1)
